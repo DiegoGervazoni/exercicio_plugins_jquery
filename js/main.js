f790557eaa21899f56cd2ba19689a500613e3e19
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-12-11 11:05:05 -0300

    adicionando validacao e messages no jquery

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function (){
+
+  $('#numeroCpf').mask('000.000.000-00', {reverse: true});
+  $('#telefone').mask('(00) 00000-0000');
+  $('#numeroCep').mask('00000-000');
+
+  $('form').validate({
+    rules: {
+      nome: {
+        required: true,
+      },
+      email: {
+        required:true,
+        email: true,
+      },
+      telefone: {
+        required:true,
+      },
+      cpf: {
+        required:true,
+      },
+      endereco: {
+        required: true,
+      },
+      cep: {
+        required: true,
+      },
+    },
+    messages: {
+      nome: '<span class="campoErrado">Por favor, insira o seu nome.</span>',
+      email:
+        '<span class="campoErrado">"Por favor, insira o seu email."</span>',
+      telefone:
+        '<span class="campoErrado">"Por favor, insira o seu telefone."</span>',
+      cpf:
+        '<span class="campoErrado">"Por favor, insira o seu CPF"</span>',
+      endereco:
+        '<span class="campoErrado">"Por favor, insira o seu endereço"</span>',
+      cep:
+        '<span class="campoErrado">"Por favor, insira o seu CEP"</span>',
+    },
+  })
+
+})
